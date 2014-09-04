167aa2acf8364776a299fbbede4e28b75e3317b3
Salvo 'LtWorf' Tomaselli <tiposchi@tiscali.it>
2014-09-04 12:14:38 +0200

    Memcache wrapper
    This is probably the peak of my intellect for today.
    
    It's a nice wrapper for memcache that lets me avoid extreme ugliness.
    
    Normal way of using memcache:
    
    content = memcache.get(key)
    if content:
        return content
    content = generate_content_dynamically()
    memcache.add(key, content)
    return content
    
    What is now possible:
    
    return memcache.get(key, generate_content_dynamically)
    
    I don't know if it's faster but it surely looks better.

---
+++ memcache.py
@@ -0,0 +1,47 @@
+from google.appengine.api import memcache
+
+
+def get(key, value_function=None, auto=True):
+    '''
+    Gets a value from memcache.
+
+    This function is a wrapper and is used to avoid
+    the ugliness of having
+
+    c = memcache.get(key)
+    if c is not None:
+        return c
+    content = generate_dynamic_content()
+    memcache.add(key, content)
+
+    This function is used in the following way:
+
+    def generator():
+        return 'dynamically generated content'
+    return memcache.get(key, generator)
+
+    Simplyifying the code.
+
+    key: well, it's self explainatory
+    value_function: callable
+        It must return a string that get()
+        will return if the item is not
+        present in cache.
+
+        If auto is true, this string will
+        be added to the cache automatically.
+    '''
+    r = memcache.get(key)
+    if r is None:
+
+        if value_function is None:
+            return None
+        else:
+            output = value_function()
+            if auto:
+                add(key, output)
+            return output
+    return r
+
+add = memcache.add
+delete = memcache.delete
